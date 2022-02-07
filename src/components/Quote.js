3bb1bf43e1260d6f47ee2ad2512c8d56f4576e35
MichaelWagnr <michaelwagner780@gmail.com>
2022-02-07 18:38:31 -0500

    Reformat

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 const Quote = () => {
 
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     // Different API endpoints to try
-    const endPointA = 'https://api.themotivate365.com/stoic-quote';
+
+    // const endPointA = 'https://api.themotivate365.com/stoic-quote';
     //CORS issue
+
     const endPointB = 'https://stoicquotesapi.com/v1/api/quotes/random';
     //Returns single quote
-    const endPointC = 'https://stoic-quotes.com/api/quotes';
+
+    // const endPointC = 'https://stoic-quotes.com/api/quotes';
     //Returns 10 quotes
-    const endPointD = 'https://randomstoicquotesapi.herokuapp.com';
+
+    // const endPointD = 'https://randomstoicquotesapi.herokuapp.com';
     //Throws JSON error
 
-    let quotes = {}
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    // Change argument to corespond to desired endpoint
 
+    // Change argument to correspond to desired endpoint
     fetch(endPointB)
         .then(data => data.json())
         .then(data => console.log(data));

b35d6b8ace6cb2bc723ba145a306fef15c412a9c
MichaelWagnr <michaelwagner780@gmail.com>
2022-02-07 20:27:11 -0500

    update monday evening

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Quote = (props) => {
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
     // Different API endpoints to try
+
 
     // const endPointA = 'https://api.themotivate365.com/stoic-quote';
     //CORS issue
@@ -15,16 +16,6 @@ const Quote = (props) => {
 
     // const endPointD = 'https://randomstoicquotesapi.herokuapp.com';
     //Throws JSON error
-
-    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-    // let results = null;
-
-    // // Change argument to correspond to desired endpoint
-    // fetch('https://stoicquotesapi.com/v1/api/quotes/random')
-    //     .then(data => data.json())
-    //     .then(data => console.log(data))
-
 
     return (
         <p>{props.quote.body} - {props.quote.author}</p>
